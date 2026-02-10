9e16477dd88f7726e93b3213043bdd4d77786843
nakanu <ampta77000@gmail.com>
2026-02-10 09:28:09 +0530

    first commit

---
+++ src/main/java/com/codewithmosh/store/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.codewithmosh.store.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("name", "Shivam");
+        return "index";
+    }
+
+
+
+}
---
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -0,0 +1,19 @@
+package com.codewithmosh.store.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+public class UserDto {
+
+    private Long id;
+    private String name;
+    private String email;
+}
